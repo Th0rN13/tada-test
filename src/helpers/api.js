c1d0a9f0ac3b9433bf99f99779516828a6b681ab
Maksim 'Th0rN13' Tolkachev <th0r@inbox.ru>
2021-04-18 11:52:40 +0500

    add websockets work

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,4 +1,6 @@
-const api_url = process.env.VUE_APP_API_URL || `https://nane.tada.team/api/`;
+const server_url = process.env.VUE_APP_SERVER_URL || 'nane.tada.team';
+const api_url = process.env.VUE_APP_API_URL || `https://${server_url}/api/`;
+const wss_url = process.env.VUE_APP_WSS_URL || `wss://${server_url}/ws`;
 
 export async function getRooms() {
 	const result = await getRequest(`${api_url}rooms`);
@@ -25,4 +27,29 @@ async function getRequest(url) {
 		console.log(err);
 		return null;
 	}
+}
+
+let ws;
+
+export function connectWebSocket(name = 'anonymous', connectCb, messageCb) {
+	ws = new WebSocket(`${wss_url}?username=${name}`);
+	ws.onopen = () => {
+		connectCb(true);
+	};
+	ws.onclose = () => {
+		connectCb(false);
+		setTimeout(() => {
+			connectWebSocket(name, connectCb, messageCb);
+		}, 500);
+	};
+	ws.onmessage = (event) => messageCb(event?.data);
+	return ws;
+}
+
+export function sendMessage(room, text, id) {
+	ws.send(JSON.stringify({ room, text, id }));
+}
+
+export function ping() {
+	ws.send(JSON.stringify({ ping: true }));
 }
